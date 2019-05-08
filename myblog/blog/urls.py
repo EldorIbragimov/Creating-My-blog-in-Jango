cd47037e4b3afb2925aa9013f719619320e8aa92
EldorIbragimov <eldor1992@mail.ru>
2019-05-08 17:56:42 +0900

    starting to write very basic parts

---
+++ myblog/blog/urls.py
@@ -0,0 +1,9 @@
+#from django.urls import path, include
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+	url(r'^$', views.home, name='blog-home')
+    #path('', views.home, name='blog-home'),
+]
