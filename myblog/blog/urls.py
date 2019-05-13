c65514657fc0a77bc5f49ddf3453cf3fbec69790
EldorIbragimov <eldor1992@mail.ru>
2019-05-13 18:59:54 +0900

    added simple blog section and about

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,9 +1,11 @@
 #from django.urls import path, include
 from django.conf.urls import url
+
 from . import views
 
 
 urlpatterns = [
-	url(r'^$', views.home, name='blog-home')
+	url(r'^$', views.home, name='blog-home'),
+	url(r'about/', views.about, name='blog-about'),
     #path('', views.home, name='blog-home'),
 ]
