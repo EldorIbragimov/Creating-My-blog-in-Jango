d5f77dba5a918ebb0eb53a53e09c0d362808e3f1
Eldor Ibragimov <eldor1992@mail.ru>
2020-01-17 09:08:13 +0000

    working on home page

--- Myblog/src/posts/models.py
+++ Myblog/src/posts/models.py
@@ -5,12 +5,14 @@ from django.urls import reverse
 
 User = get_user_model()
 
+
 class PostView(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username        
+        return self.user.username
+
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -19,17 +21,20 @@ class Author(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Category(models.Model):
     title = models.CharField(max_length=20)
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    post = models.ForeignKey('Post', related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        'Post', related_name='comments', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
@@ -40,44 +45,43 @@ class Post(models.Model):
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     content = HTMLField()
-    #comment_count = models.IntegerField(default = 0)
-    #view_count = models.IntegerField(default = 0)
+    # comment_count = models.IntegerField(default = 0)
+    # view_count = models.IntegerField(default = 0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
-    previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
-    next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
+    previous_post = models.ForeignKey(
+        'self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
+    next_post = models.ForeignKey(
+        'self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={
-            'id': self.id
+            'pk': self.pk
         })
-    
+
     def get_update_url(self):
         return reverse('post-update', kwargs={
-            'id': self.id
+            'pk': self.pk
         })
 
     def get_delete_url(self):
         return reverse('post-delete', kwargs={
-            'id': self.id
+            'pk': self.pk
         })
-    
+
     @property
     def get_comments(self):
         return self.comments.all().order_by('-timestamp')
 
     @property
-    def view_count(self):
-        return PostView.objects.filter(post=self).count()
-
-    @property
     def comment_count(self):
         return Comment.objects.filter(post=self).count()
 
-
-
+    @property
+    def view_count(self):
+        return PostView.objects.filter(post=self).count()
